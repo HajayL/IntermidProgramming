0ed257b17e08665d2f44a41e1f358bca323f4691
HajayL <hajaylawson10@gmail.com>
2024-01-23 11:46:59 -0700

    Stopwatch

--- OpenForms/Form1.Designer.cs
+++ OpenForms/Form1.Designer.cs
@@ -29,12 +29,226 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.resume = new System.Windows.Forms.Button();
+            this.lap = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(262, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(222, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // stop
+            // 
+            this.stop.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.stop.Location = new System.Drawing.Point(262, 316);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(222, 23);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "STOP";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // resume
+            // 
+            this.resume.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resume.Location = new System.Drawing.Point(262, 287);
+            this.resume.Name = "resume";
+            this.resume.Size = new System.Drawing.Size(222, 23);
+            this.resume.TabIndex = 1;
+            this.resume.Text = "RESUME";
+            this.resume.UseVisualStyleBackColor = true;
+            this.resume.Click += new System.EventHandler(this.resume_Click);
+            // 
+            // lap
+            // 
+            this.lap.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lap.Location = new System.Drawing.Point(262, 345);
+            this.lap.Name = "lap";
+            this.lap.Size = new System.Drawing.Size(222, 23);
+            this.lap.TabIndex = 3;
+            this.lap.Text = "LAP";
+            this.lap.UseVisualStyleBackColor = true;
+            this.lap.Click += new System.EventHandler(this.lap_Click);
+            // 
+            // reset
+            // 
+            this.reset.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.reset.Location = new System.Drawing.Point(262, 374);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(222, 23);
+            this.reset.TabIndex = 4;
+            this.reset.Text = "RESET";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(53, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "White";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Grey";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(24, 67);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(52, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Black";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(593, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Background Color";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(24, 67);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(52, 17);
+            this.radioButton4.TabIndex = 2;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Black";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Click += new System.EventHandler(this.radio2Button_Click);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(24, 43);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(47, 17);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Grey";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.Click += new System.EventHandler(this.radio2Button_Click);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(24, 19);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(53, 17);
+            this.radioButton6.TabIndex = 0;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "White";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.Click += new System.EventHandler(this.radio2Button_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Location = new System.Drawing.Point(593, 215);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "TextBox Background Color";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.lap);
+            this.Controls.Add(this.resume);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button resume;
+        private System.Windows.Forms.Button lap;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- OpenForms/Form1.cs
+++ OpenForms/Form1.cs
@@ -1,20 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace OpenForms
 {
     public partial class Form1 : Form
     {
+        Stopwatch time;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            time = new Stopwatch();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = $"{time.ElapsedMilliseconds} ms";
+        }
+
+        private void resume_Click(object sender, EventArgs e)
+        {
+            time.Start();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            time.Stop();
+        }
+
+        private void lap_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add($"{(double)time.ElapsedMilliseconds / 1000}");
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            time.Reset();
+            listBox1.Items.Clear();
+        }
+
+        private void radioButton_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                this.BackColor = Color.White;
+                this.ForeColor = Color.Black;
+                groupBox1.ForeColor = Color.Black;
+                groupBox2.ForeColor = Color.Black;
+                resume.BackColor = Color.White;
+                stop.BackColor = Color.White;
+                lap.BackColor = Color.White;
+                reset.BackColor = Color.White;
+            }
+            else if (radioButton2.Checked)
+            {
+                this.BackColor = Color.Gray;
+                this.ForeColor = Color.Black;
+                groupBox1.ForeColor = Color.Black;
+                groupBox2.ForeColor = Color.Black;
+                resume.BackColor = Color.White;
+                stop.BackColor = Color.White;
+                lap.BackColor = Color.White;
+                reset.BackColor = Color.White;
+            }
+            else if (radioButton3.Checked)
+            {
+                this.BackColor = Color.Black;
+                this.ForeColor = Color.White;
+                groupBox1.ForeColor = Color.White;
+                groupBox2.ForeColor = Color.White;
+                resume.BackColor = Color.Gray;
+                stop.BackColor = Color.Gray;
+                lap.BackColor = Color.Gray;
+                reset.BackColor = Color.Gray;
+            }
+        }
+
+        private void radio2Button_Click(object sender, EventArgs e)
+        {
+            if (radioButton6.Checked)
+            {
+                listBox1.BackColor = Color.White;
+                listBox1.ForeColor = Color.Black;
+            }
+            else if (radioButton5.Checked)
+            {
+                listBox1.BackColor = Color.Gray;
+                listBox1.ForeColor = Color.Black;
+            }
+            else if (radioButton4.Checked)
+            {
+                listBox1.BackColor = Color.Black;
+                listBox1.ForeColor = Color.White;
+            }
         }
     }
 }
